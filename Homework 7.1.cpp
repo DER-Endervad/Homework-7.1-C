4ca22454c7a2d124c5e9559ee58df09e117c0208
Даниил <derendervad@gmail.com>
2022-12-08 19:31:17 +0300

    change

--- Homework 7.1.cpp
+++ Homework 7.1.cpp
@@ -1,26 +1,37 @@
 ﻿#include <iostream>
 #include <Windows.h>
 #define MODE 2
+#ifndef MODE
+#error MODE не обьявлен
+#else
+
+
+void add();
 int main()
 {
     setlocale(LC_ALL, "Russian");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-#ifdef MODE
     #if MODE == 0
         std::cout << "Работаю в режиме тренировки" << std::endl;
     #elif MODE == 1
         std::cout << "Работаю в боевом режиме" << std::endl;
-        int one = 0, two = 0;
-        std::cout << "Введите число 1: ";
-        std::cin >> one;
-        std::cout << "Введите число 2: ";
-        std::cin >> two;
-        std::cout << "Результат сложения: " << one + two << std::endl;
+        add();
     #else
         std::cout << "Неизвестный режим. Завершение работы" << std::endl;
     #endif
-#else
-    #error MODE не обьявлен
-#endif
 }
+
+#if MODE == 1
+void add() {
+    int one = 0, two = 0;
+    std::cout << "Введите число 1: ";
+    std::cin >> one;
+    std::cout << "Введите число 2: ";
+    std::cin >> two;
+    std::cout << "Результат сложения: " << one + two << std::endl;
+}
+#endif
+
+
+#endif
